8dfd6ffb276b2b7966bb10ab2906496c54c10fb1
zflint <32756823+jzfcoder@users.noreply.github.com>
2020-11-27 16:31:44 -0800

    FirstIterationDetermineSourceBias

---
+++ src/bias.py
@@ -0,0 +1,54 @@
+from tld import get_tld
+
+import sqlite3
+from sqlite3 import Error
+
+import os
+
+link = input("Enter Article Link Here: ")
+#link = "http://abcnews.go.com"
+domain = get_tld(link, as_object=True)
+print("We found this domain from your link: " + domain.fld) # --> www.example.test
+
+def create_connection(db_file):
+
+    print("Creating Connection ...")
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def select_bias(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT BiasRating FROM BiasDB WHERE Domain like '%" + domain.fld + "%' ")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row[0])
+
+def main():
+    database = r"C:\Users\timfl\Documents\GitHub\MisinformationProject\sqlite\BiasDB.db"
+    
+    # print(os.path.abspath('.'))
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        print(domain.fld + "'s bias is...")
+        select_bias(conn)
+
+if __name__ == '__main__':
+    main()
